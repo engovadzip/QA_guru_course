14b2505b197336ec00092e048dff45649998ef2e
Engo Vadzip <abzxcvvcxzba@yandex.ru>
2023-10-14 18:02:12 +0300

    Complete task

---
+++ Homework_7/utils.py
@@ -0,0 +1,12 @@
+import os
+
+
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources')
+TMP_PATH = os.path.join(PROJECT_ROOT_PATH, 'tmp')
+ZIP_PATH = os.path.join(TMP_PATH, 'HW.zip')
+
+PDF_FILE = 'Python Testing with Pytest (Brian Okken).pdf'
+TXT_FILE = 'Hello.txt'
+XLS_FILE = 'file_example_XLS_10.xls'
+XLSX_FILE = 'file_example_XLSX_50.xlsx'
