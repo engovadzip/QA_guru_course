ed5df9878b662995a74cb1f4fe522421102b419f
Engo Vadzip <abzxcvvcxzba@yandex.ru>
2023-10-31 01:41:15 +0300

    Initial version

---
+++ Homework_10/high/test_hw10_high.py
@@ -0,0 +1,28 @@
+import allure
+from Homework_10.high.pages.registration_page_high import RegistrationPage
+from Homework_10.high.users.users import User
+
+
+@allure.step("Проверка формы регистрации")
+def test_hw10():
+    registration_page = RegistrationPage()
+    user = User(
+        last_name='Gazizov',
+        first_name='Timir',
+        email='my_mail@gmail.com',
+        gender='Male',
+        phone='9999999999',
+        month_of_birth='June',
+        year_of_birth='1994',
+        day_of_birth='11',
+        subject='Maths',
+        hobby='Sports, Music',
+        picture='picture.jpg',
+        address='Shevchenko street',
+        state='Haryana',
+        city='Karnal'
+    )
+
+    registration_page.open_form()
+    registration_page.register_user(user)
+    registration_page.check_registration(user)
