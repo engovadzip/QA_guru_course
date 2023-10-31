8b87c986bc8bed8dcb156ad428879bde07eefc8b
Engo Vadzip <abzxcvvcxzba@yandex.ru>
2023-11-01 00:17:00 +0300

    Remove repos

--- Homework_2/conftest.py
+++
@@ -1,9 +0,0 @@
-from selene.support.shared import browser
-import pytest
-
-@pytest.fixture(scope="session", autouse=True)
-def whole_window():
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
-    yield
-    browser.quit()
