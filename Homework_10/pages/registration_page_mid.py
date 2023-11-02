c8a8ff7d4ea3ccb188d78b615f86be3bf105e615
Engo Vadzip <abzxcvvcxzba@yandex.ru>
2023-11-02 13:07:30 +0300

    Update code

--- Homework_10/pages/registration_page_mid.py
+++ Homework_10/pages/registration_page_mid.py
@@ -8,10 +8,10 @@ class RegistrationPage():
         browser.open('/automation-practice-form')
 
     def fill_first_name(self, name):
-        browser.element('//input[@placeholder="First Name"]').send_keys(name)
+        browser.element('#firstName').send_keys(name)
 
     def fill_last_name(self, last_name):
-        browser.element('//input[@placeholder="Last Name"]').send_keys(last_name)
+        browser.element('#lastName').send_keys(last_name)
 
     def fill_email(self, email):
         browser.element('#userEmail').send_keys(email)
@@ -21,19 +21,19 @@ class RegistrationPage():
         browser.element(f'//label[text() = "{gender}"]').click()
 
     def fill_phone_number(self, phone):
-        browser.element('//input[@placeholder="Mobile Number"]').send_keys(phone)
+        browser.element('#userNumber').send_keys(phone)
 
     def select_date_of_birth(self, month, day, year):
         month = month.capitalize()
         browser.element('#dateOfBirthInput').click()
-        browser.element('//select[@class="react-datepicker__month-select"]').click()
+        browser.element('.react-datepicker__month-select').click()
         browser.element(f'//*[text()="{month}"]').click()
 
-        browser.element('//select[@class="react-datepicker__year-select"]').click()
+        browser.element('.react-datepicker__year-select').click()
         browser.element(f'//*[text()={year}]').click()
         browser.element(f'//div[text()={day}]').click()
 
-        browser.element('//input[@placeholder="First Name"]').click()
+        browser.element('#firstName').click()
 
     def select_subject(self, subject):
         browser.element('#subjectsInput').type(f'{subject}').press_enter()
@@ -46,16 +46,16 @@ class RegistrationPage():
         browser.element('#uploadPicture').should(be.blank).send_keys(resources.path('picture.jpg'))
 
     def fill_address(self, address):
-        browser.element('//textarea[@placeholder="Current Address"]').send_keys(f'{address}')
+        browser.element('#currentAddress').send_keys(f'{address}')
 
     def select_state(self, state):
         state = state.capitalize()
-        browser.element('//div[text()="Select State"]').click()
+        browser.element('#state').click()
         browser.element(f'//*[text()="{state}"]').click()
 
     def select_city(self, city):
         city = city.capitalize()
-        browser.element('//div[text()="Select City"]').click()
+        browser.element('#city').click()
         browser.element(f'//*[text()="{city}"]').click()
 
     def submit_button_click(self):
