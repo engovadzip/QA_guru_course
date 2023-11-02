7212fd2977ac489f09618fb452b0582b7b6e827e
Engo Vadzip <abzxcvvcxzba@yandex.ru>
2023-11-02 13:40:45 +0300

    Update structure

--- Homework_10/objects/pages/registration_page_high.py
+++ Homework_10/objects/pages/registration_page_high.py
@@ -1,6 +1,5 @@
 from selene import browser, be, have
-import os
-
+import resources
 
 
 
@@ -9,41 +8,39 @@ class RegistrationPage():
         browser.open('/automation-practice-form')
 
     def register_user(self, user):
-        browser.element('//input[@placeholder="First Name"]').send_keys(user.first_name)
-        browser.element('//input[@placeholder="Last Name"]').send_keys(user.last_name)
+        browser.element('#firstName').send_keys(user.first_name)
+        browser.element('#lastName').send_keys(user.last_name)
         browser.element('#userEmail').send_keys(user.email)
 
         user.gender = user.gender.capitalize()
         browser.element(f'//label[text() = "{user.gender}"]').click()
-        browser.element('//input[@placeholder="Mobile Number"]').send_keys(user.phone)
+        browser.element('#userNumber').send_keys(user.phone)
 
         user.month_of_birth = user.month_of_birth.capitalize()
         browser.element('#dateOfBirthInput').click()
-        browser.element('//select[@class="react-datepicker__month-select"]').click()
+        browser.element('.react-datepicker__month-select').click()
         browser.element(f'//*[text()="{user.month_of_birth}"]').click()
-        browser.element('//select[@class="react-datepicker__year-select"]').click()
+        browser.element('.react-datepicker__year-select').click()
         browser.element(f'//*[text()={user.year_of_birth}]').click()
         browser.element(f'//div[text()={user.day_of_birth}]').click()
 
-        browser.element('//input[@placeholder="First Name"]').click()
+        browser.element('#firstName').click()
 
         browser.element('#subjectsInput').type(f'{user.subject}').press_enter()
 
         browser.element('//label[@for="hobbies-checkbox-1"]').click()
         browser.element('//label[@for="hobbies-checkbox-3"]').click()
 
-        current_dir = os.path.abspath(os.path.dirname(__file__))
-        file_path = os.path.join(current_dir, 'picture.jpg')
-        browser.element('#uploadPicture').should(be.blank).send_keys(file_path)
+        browser.element('#uploadPicture').should(be.blank).send_keys(resources.path('picture.jpg'))
 
-        browser.element('//textarea[@placeholder="Current Address"]').send_keys(f'{user.address}')
+        browser.element('#currentAddress').send_keys(f'{user.address}')
 
         user.state = user.state.capitalize()
-        browser.element('//div[text()="Select State"]').click()
+        browser.element('#state').click()
         browser.element(f'//*[text()="{user.state}"]').click()
 
         user.city = user.city.capitalize()
-        browser.element('//div[text()="Select City"]').click()
+        browser.element('#city').click()
         browser.element(f'//*[text()="{user.city}"]').click()
 
         browser.element('#submit').click()
